ab818279ef234280c586be67f207c706960257f1
Dimitri Ravot <58395707+Mr-Fakename@users.noreply.github.com>
2025-10-17 23:17:42 +0200

    Upgrade version of Core and Dashboard to 3.22

--- src/components/Datagrid/customCells/useCustomCellRenderers.ts
+++ src/components/Datagrid/customCells/useCustomCellRenderers.ts
@@ -26,7 +26,7 @@ export function useCustomCellRenderers() {
       dateCellRenderer(locale),
       dropdownCellRenderer,
       thumbnailCellRenderer,
-      ...customRenderers,
+      ...(customRenderers ?? []),
     ],
     [customRenderers, locale],
   );
